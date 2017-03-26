5e282c536d38ecd8b6929476767b1c7b39d7a742
zedong <lycczzd@gmail.com>
2017-03-26 22:28:50 +0800

    implement groups#edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,9 +14,16 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.save
-    redirect_to groups_path
+    redirect_to groups_path , notice: "Create Group Success!"
   end
-
+  def edit
+    @group = Group.find(params[:id])
+  end
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path, notice: "Update Group Success!"
+  end
 
   private
   def group_params
