f404c839e32c9b487b4033cc3d89e8f7540b1f42
zedong <lycczzd@gmail.com>
2017-03-27 22:54:46 +0800

    implement edit button in Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -14,7 +15,22 @@ class PostsController < ApplicationController
       render :new
     end
   end
-
+  def edit
+    @post = Post.find(params[:id])
+    @group = Group.find(params[:group_id])
+    if current_user != @post.user
+      redirect_to account_posts_path, warning: "You have no permission!"
+    end
+  end
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success!"
+    else
+      render :edit
+    end
+  end
   private
   def post_params
     params.require(:post).permit(:content)
